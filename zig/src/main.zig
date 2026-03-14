4f00b8d0d929b0abe7581aac789e21738617b471
niico <5653050+niicojs@users.noreply.github.com>
2026-03-14 14:07:39 +0100

    fix compile

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -15,7 +15,7 @@ pub fn main(init: std.process.Init) !void {
 
     const io = init.io;
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
